e3a9caf673b4c95cc1a14795533e669ebf88f38c
N. Frazier <nfrazier@rice.edu>
2017-04-18 12:12:17 -0500

    updated for initial condition of nworkers() = nprocs()

--- src/SetWorkers.jl
+++ src/SetWorkers.jl
@@ -7,16 +7,17 @@ module SetWorkers
   	)
   	## FIX NUMBER OF WORKERS
   	# they split the run of simulations
-  	current_list = workers()
-  	if nworkers()<(PROCS)
-  		println("Adding $(PROCS-nworkers()) workers to estimation suite...\n")
-  		addprocs(PROCS-nworkers())
+  	current_wrkrs = (nprocs()==1) ? 0 : nworkers()
+  	if (current_wrkrs<PROCS)
+  		println("Adding $(PROCS-current_wrkrs) workers to estimation suite...\n")
+  		addprocs(PROCS-current_wrkrs)
   	end
-  	if nworkers() > PROCS
+  	if (current_wrkrs > PROCS)
   		a = procs()
-  		println("Extra workers found. Removing $(nworkers()-PROCS) workers...\n")
-  		rmprocs(a[2:(nworkers()-PROCS)])
+  		println("Extra workers found. Removing $(current_wrkrs-PROCS) workers...\n")
+  		rmprocs(a[2:(current_wrkrs-PROCS)])
   	end
+    return
   end
 
 end
